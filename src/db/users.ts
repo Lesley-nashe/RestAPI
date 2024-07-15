cd312cbb3bcef4aa3aafc4cea0ed13c9182aed56
Tadiwa Lesley Musekiwa <lesliemusekiwa@gmail.com>
2024-07-15 11:28:15 +0200

    Created User Schema

--- src/db/users.ts
+++ src/db/users.ts
@@ -11,3 +11,19 @@ const UserSchema = new Schema({
 })
 
 export const UserModal  = mongoose.model('User', UserSchema);
+
+export const getUsers = () => UserModal.find();
+
+export const getUsersByEmail = (email: String) => UserModal.findOne({ email });
+
+export const getUsersBySessionToken = (sessionToken: String) => UserModal.findOne({
+    'authentication.sessionToken' : sessionToken,
+})
+
+export const getUserById  = (Id: String) => UserModal.findOne(Id);
+export const creatUser = (values: Record<string, any>) => new UserModal(values).save().then((
+    user) => user.toObject());
+
+export const deleteUser = (Id: String) => UserModal.findByIdAndDelete({ _Id: Id});
+
+export const updateUser = (Id: String, values: Record<string, any> ) => UserModal.findByIdAndUpdate({Id, values});
